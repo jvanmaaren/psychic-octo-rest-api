ce6b66fda4bbe8daaac895119113d6ebee44d34e
Jorg van Maaren <j.vanmaaren@labdigital.nl>
2023-06-22 15:21:09 +0200

    feat: Setup simple subscriber api
    Added test routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,23 +1,80 @@
 const express = require("express");
+const Subscriber = require("../models/subscriber");
 
 const router = express.Router();
 
 // Getting all
-router.get("/", (req, res) => {
-  res.send("Hello World");
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Getting one
-router.get("/:id", (req, res) => {});
+router.get("/:id", getSubscriberMiddleware, (req, res) => {
+  res.json(res.subscriber);
+});
 
 // Creating one
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const action = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+
+  try {
+    const newSubscriber = await action.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // Updating one
-// PUT would update all info at once. PATCH would update only the info that was sent.
-router.patch("/:id", (req, res) => {});
+/* PUT would update all info at once. PATCH would update only the info that was sent. */
+router.patch("/:id", getSubscriberMiddleware, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
+});
 
 // Deleting one
-router.patch("/:id", (req, res) => {});
+router.delete("/:id", getSubscriberMiddleware, async (req, res) => {
+  try {
+    await res.subscriber.deleteOne();
+    res.json({ message: "Deleted subscriber" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+
+// Middleware
+async function getSubscriberMiddleware(req, res, next) {
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+
+    if (!subscriber) {
+      return res.status(404).json({ message: "Can not find subscriber" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;
